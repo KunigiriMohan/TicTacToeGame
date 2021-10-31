48c41a2671ece6ac96a703fa96a769f8591df55d
KunigiMohan <kmohanr5@gmail.com>
2021-10-31 18:34:06 +0530

    Added : Creating ability to choose position in board for player

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,23 +1,40 @@
+import java.util.Scanner;
 
-class TicTacToeUC3
-{
-    public static void main(String[] args)
-    {
-        showBoard();                                        //Calling showBoard() method
+public class TicTacToeGame {
+    static Scanner input = new Scanner(System.in);
 
+    public static void main(String[] args) {
+
+        char[] board = new char[10];        //creating board array of length 10
+        showBoard(board);
+        char inputChoice = chooseLetter();      //storing symbol choosen by user to inputChoice variable
+        for(int k=0;k<=9;k++) {
+            int i = input.nextInt();
+            board[i] = inputChoice;
+            showBoard(board);
+        }
+    }
+    /*
+     * Creating showBoard() to show TicTacToe Game board to user
+     * */
+
+    public static void showBoard(char[] board) {
+        System.out.println("\n" + "Displaying the Tic Tac Toe Board");
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("-----------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("-----------");
+        System.out.println(board[7] + " | " + board[8] + " |" + board[9]);
+        System.out.println("-----------");
     }
 
     /*
-     * Creating showBoard() method to show TicTacToe game board to user
+     * Creating chooseLetter() method to choose which symbol user method
      * */
 
-    public static void showBoard(){
-        System.out.println("board 1 "+" | "+"board 2 "+ " | "+ " board 3 ");
-        System.out.println("-------------------------------   ");
-
-        System.out.println("board 4 "+" | "+"board 5 "+ " | "+ " board 6 ");
-        System.out.println("-------------------------------");
-
-        System.out.println("board 7 "+" | "+"board 8 "+ " | "+ " board 9 ");
+    public static char chooseLetter() {
+        System.out.println("\n" + "Enter your choice: X | O");
+        char choice = input.next().charAt(0);
+        return choice;                                      //chooseLetter method returning choice of user
     }
 }
